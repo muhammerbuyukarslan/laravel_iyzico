0e89b3a60bab189fa7dc3fca553b8c75daa0e3a4
seinei <muhammerbarslan@gmail.com>
2022-12-18 22:36:36 +0300

    Kullanıcılar Tablosu Yapma

---
+++ resources/views/backend/users/index.blade.php
@@ -0,0 +1,113 @@
+
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashbord Users Page</title>
+
+    <link rel="stylesheet" href="{{asset("css/app.css")}}">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset("css/dashboard.css")}}" rel="stylesheet">
+</head>
+<body>
+
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">Laravel Ecommerce</a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <div class="navbar-nav">
+        <div class="nav-item text-nowrap">
+            <a class="nav-link px-3" href="#">Çıkış</a>
+        </div>
+    </div>
+</header>
+
+<div class="container-fluid">
+    <div class="row">
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+            <div class="position-sticky pt-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">
+                            <span data-feather="home"></span>
+                            Yönetim Paneli
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/users">
+                            <span data-feather="file"></span>
+                            Kullanıcılar
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h2">Yönetim paneli</h1>
+            </div>
+            <h2>Kullanıcılar</h2>
+            <div class="table-responsive">
+                <table class="table table-striped table-sm">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Ad Soyad</th>
+                        <th scope="col">Eposta</th>
+                        <th scope="col">Durum</th>
+                        <th scope="col">İşlemler</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @if(count($users)>0)
+                        @foreach($users as $user)
+                    <tr>
+                        <td>{{$loop->iteration}}</td>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->is_active}}</td>
+                        <td>Güncelle Sil Değiştir</td>
+                    </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td colspan="5">
+                                <p class="text-center">Herhangi bir kullanıcı bulunamadı</p>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                    @endif
+                </table>
+            </div>
+        </main>
+    </div>
+</div>
+
+<script type="text/javascript" src="{{asset("js/app.js")}}"></script>
+
+</body>
+</html>
+
