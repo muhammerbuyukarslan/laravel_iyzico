f23048b83bc86c9c3348c200e01291d78ba399ce
seinei <muhammerbarslan@gmail.com>
2022-12-23 08:08:03 +0300

    Kullanıcı Kayıt

--- resources/views/backend/users/insert_form.blade.php
+++ resources/views/backend/users/insert_form.blade.php
@@ -55,7 +55,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/users">
+                        <a class="nav-link" {{Str::of(url()->current())->contains("/users") ? "active" : ""}} href="{{url("/users")}}">
                             <span data-feather="file"></span>
                             Kullanıcılar
                         </a>
@@ -73,59 +73,54 @@
                     </div>
                 </div>
             </div>
+            <h2>Yeni Kullanıcı Ekle</h2>
             <div class="table-responsive">
-                <table class="table table-striped table-sm">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Ad Soyad</th>
-                        <th scope="col">Eposta</th>
-                        <th scope="col">Durum</th>
-                        <th scope="col">İşlemler</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @if(count($users)>0)
-                        @foreach($users as $user)
-                    <tr>
-                        <td>{{$loop->iteration}}</td>
-                        <td>{{$user->name}}</td>
-                        <td>{{$user->email}}</td>
-                        <td>{{$user->is_active}}</td>
-                        <td>
-                            <ul class="nav float-start">
-                                <li class="nav-item">
-                                    <a class="nav-link text-black" href="{{url("/users/$user->user_id/edit")}}">
-                                        <span data-feather="edit"></span>
-                                        Güncelle
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link list-item-delete text-black" href="{{url("/users/$user->user_id")}}">
-                                        <span data-feather="trash-2"></span>
-                                        Sil
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-black" href="/users">
-                                        <span data-feather="lock"></span>
-                                        Şifre Değiştir
-                                    </a>
-                                </li>
-                            </ul>
-                        </td>
-                    </tr>
-                        @endforeach
-                    @else
-                        <tr>
-                            <td colspan="5">
-                                <p class="text-center">Herhangi bir kullanıcı bulunamadı</p>
-                            </td>
-                        </tr>
-
-                    </tbody>
-                    @endif
-                </table>
+                <form action="{{url("/users")}}" METHOD="POST">
+                    @csrf
+                    <div class="row">
+                        <div class="col-lg-6">
+                                <label for="name" class="form-label">Ad Soyad</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Ad Soyad Giriniz">
+                        </div>
+                        <div class="col-lg-6">
+                                <label for="email" class="form-label">Eposta</label>
+                                <input type="text" class="form-control" id="email" name="email" placeholder="Eposta Giriniz">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <label for="password" class="form-label">Şifre Giriniz</label>
+                            <input type="password" class="form-control" id="password" name="password" placeholder="Şifre Giriniz">
+                        </div>
+                        <div class="col-lg-6">
+                            <label for="password2" class="form-label">Şifre Tekrarı</label>
+                            <input type="password" class="form-control" id="password2" name="password2" placeholder="Şifrenizi Tekrar Giriniz">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-check mt-3">
+                                <input class="form-check-input" type="checkbox" id="is_admin" name="is_admin">
+                                <label class="form-check-label" for="is_admin">
+                                    Yetkili Kullanıcı
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-check mt-3">
+                                <input class="form-check-input" type="checkbox" id="is_active" name="is_active">
+                                <label class="form-check-label" for="is_active">
+                                    Aktif Kullanıcı
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-success mt-2">KAYDET</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </main>
     </div>
